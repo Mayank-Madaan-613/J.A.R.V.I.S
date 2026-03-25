1d46c653beba5de3104c79a6c7648bee8d068257
Mayank <mayankmadaan613@gmail.com>
2026-03-25 18:03:05 +0530

    update API

--- geminiapi.py
+++ geminiapi.py
@@ -4,7 +4,7 @@ import os
 load_dotenv()
 def AI_search(ques):
     api_key=os.getenv("API_Gemini_key")
-    gem_obj=genai.Client(api_key=api_key)#AIzaSyCgFuTiOVp20axTNtw_KMuU5l85YvGGhec
+    gem_obj=genai.Client(api_key=api_key)
     querry=ques+"?"+"explain in hardly 3 lines"
     response=gem_obj.models.generate_content(model="gemini-3-flash-preview",contents=querry)
     out=response.text    
