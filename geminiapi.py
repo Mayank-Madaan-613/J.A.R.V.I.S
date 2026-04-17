cfc65837f2fb8a412b6802e5692a1fb126fb505a
Mayank <mayankmadaan613@gmail.com>
2026-04-17 17:16:40 +0530

    Clean Dead code

--- geminiapi.py
+++ geminiapi.py
@@ -5,7 +5,7 @@ load_dotenv()
 def AI_search(ques):
     api_key=os.getenv("API_Gemini_key")
     gem_obj=genai.Client(api_key=api_key)
-    querry=ques+"?"+"explain in hardly 3 lines"
+    querry=ques+"?"+"explain extrermely short"
     response=gem_obj.models.generate_content(model="gemini-3-flash-preview",contents=querry)
     out=response.text    
     return out
