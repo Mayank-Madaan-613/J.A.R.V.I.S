618b2c278619411a1be956bf756251d9fd5d4755
Mayank <mayankmadaan613@gmail.com>
2026-06-28 19:14:10 +0530

    Refactor: upgrade to OOP structure

--- geminiapi.py
+++ geminiapi.py
@@ -2,10 +2,17 @@ from dotenv import load_dotenv
 from google import genai
 import os
 load_dotenv()
-def AI_search(ques):
-    api_key=os.getenv("API_Gemini_key")
-    gem_obj=genai.Client(api_key=api_key)
-    querry=ques+"?"+"explain extrermely short"
-    response=gem_obj.models.generate_content(model="gemini-3-flash-preview",contents=querry)
-    out=response.text    
-    return out
+class gemini:
+    response=""
+    def __init__(self,ques):
+        api_key=os.getenv("API_Gemini_key")
+        gem_obj=genai.Client(api_key=api_key)
+        querry=ques+"?"+"explain extrermely short"
+        response=gem_obj.models.generate_content(model="gemini-3-flash-preview",contents=querry)
+        out=response.text 
+        final_response=""
+        for i in out:
+            if i not in "*-#.!":
+                final_response+=i
+        self.response=final_response
+
