c6512da2558afba750a95284d3b584fbadd05582
Mayank <mayankmadaan613@gmail.com>
2026-07-05 18:02:32 +0530

    Chore: clean dead code

--- tts.py
+++ tts.py
@@ -2,27 +2,6 @@ import sounddevice as sd
 import numpy as np
 from piper.voice import PiperVoice
 
-# fs=24000
-# sd.default.samplerate = 44100
-# sd.default.channels = 1
-# audio=sd.rec(3*fs)
-# sd.wait()
-# print("playing recording:")
-# sd.play(audio,fs)
-# sd.wait()
-
-# for gs,ps,audio in generator:
-#     print(audio.shape)
-#     print(audio.dtype)
-
-# try:
-#     result = next(generator)
-#     print("Success!")
-#     print(result)
-# except StopIteration:
-#     print("Generator produced nothing.")
-# except Exception as e:
-#     print(type(e).__name__, e)
 class TTS:
     def __init__(self):
         self.stream =sd.OutputStream(samplerate=22050,channels=1,dtype=np.int16)
@@ -34,4 +13,3 @@ class TTS:
     def close(self):
         self.stream.stop()
         self.stream.close()
-
