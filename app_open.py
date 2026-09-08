9b41e93db6c5f6692bcdcb8b1f0ff899fe85ce7e
Mayank <mayankmadaan613@gmail.com>
2026-09-08 17:07:41 +0530

    feat: if app not installed in windows then opens the website for that particular app

--- app_open.py
+++ app_open.py
@@ -1,13 +1,19 @@
 from AppOpener import open
 import re
+from websearch import Browser
 class app_open:
-    def __init__(self,query):
+    def open(self,query):
+        self.browser=Browser()
         pattern_website=( r'\b(?:open|fireup|launch|visit|go\s+to)?\s*' r'[\s,.:;-]*'r'(?:the\s+)?'r'(?:website\s+)?'r'([a-zA-Z0-9.-]+)'r'(?:\s+website)?\b')
         self.app=re.search(pattern_website,query,re.IGNORECASE)
         if self.app:
-            open(self.app.group(1), match_closest=True)
-            self.msg=""
+            try:
+                open(self.app.group(1), throw_error=True)
+                return ""
+            except:
+                self.browser.open_site(self.app.group(1))
+                return ""
         else:
-            self.msg="Sorry i could not find the App."
+            return "Sorry i could not find the App."
 
 
